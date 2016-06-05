3da58c8b1a9f4c165d47b2a1569e6f1d499f2546
Varun, Joshi <Rorschak@apples-MacBook-Pro.local>
2016-06-06 01:20:51 +0530

    Creating a generic request class to connect to server
    Following the volley library and creating a generic class to handle the
    requests

---
+++ app/src/main/java/com/timer/rorschak/dogather/Requests/ServerGenericRequest.java
@@ -0,0 +1,25 @@
+package com.timer.rorschak.dogather.Requests;
+
+
+import com.android.volley.toolbox.JsonRequest;
+
+/**
+ * ServerGenericRequest.java - Generic request to be sent to server
+ * @author  Rorschak1
+ * @version 1.0
+ */
+public class ServerGenericRequest {
+
+    private String url;
+
+    public ServerGenericRequest (String urlstring) {
+
+        url=urlstring;
+    }
+
+
+
+
+
+
+}
